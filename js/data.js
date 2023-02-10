09ef93ca03bbd912d008cbb1c65dd06317383ba3
Julius Beer <julius.beerlin@gmail.com>
2023-02-10 12:44:07 +0100

    Cards and data added to cards. Style to center

--- js/data.js
+++ js/data.js
@@ -3,54 +3,63 @@ let toDos = `[
 "task":"take the dog for a walk",
 "info":"go to Prater",
 "priority":0,
-"deadline":12.2.2023
+"deadline":"12.2.2023",
+"pic":"https://cdn.pixabay.com/photo/2015/12/01/20/28/forest-1072828_1280.jpg"
     },
     {
         "task":"go shopping for the week",
         "info":"at Hofer",
         "priority":0,
-        "deadline":23.2.2023
+        "deadline":"23.2.2023",
+        "pic":"https://cdn.pixabay.com/photo/2016/03/23/19/38/shopping-carts-1275480_1280.jpg"
             },
             {
                 "task":"go to the gym",
                 "info":"Holmesplace",
                 "priority":0,
-                "deadline":25.2.2023
+                "deadline":"25.2.2023",
+                "pic":"https://cdn.pixabay.com/photo/2016/12/04/22/15/fitness-1882721_1280.jpg"
                     },
                     {
                         "task":"cook for tomorrow",
                         "info":"Lasagne",
                         "priority":0,
-                        "deadline":16.2.2023
+                        "deadline":"16.2.2023",
+                        "pic":"https://cdn.pixabay.com/photo/2017/02/15/10/39/salad-2068220_1280.jpg"
                             },
                             {
                                 "task":"Meet Jan",
                                 "info":"Eiles",
                                 "priority":0,
-                                "deadline":24.2.2023
+                                "deadline":"24.2.2023",
+                                "pic":"https://cdn.pixabay.com/photo/2019/07/17/11/02/neon-sign-4343663_1280.jpg"
                                     },
                                     {
                                         "task":"Buy a scale",
                                         "info":"Media Markt",
                                         "priority":0,
-                                        "deadline":15.2.2023
+                                        "deadline":"15.2.2023",
+                                        "pic":"https://cdn.pixabay.com/photo/2014/09/20/13/52/board-453758_1280.jpg"
                                             },
                                             {
                                                 "task":"fuel the car",
                                                 "info":"Jet",
                                                 "priority":0,
-                                                "deadline":12.2.2023
+                                                "deadline":"12.2.2023",
+                                                "pic":"https://cdn.pixabay.com/photo/2016/12/17/20/06/gas-pump-1914310_1280.jpg"
                                                     },
                                                     {
                                                         "task":"Go for a hike",
                                                         "info":"Rax",
                                                         "priority":0,
-                                                        "deadline":15.2.2023
+                                                        "deadline":"15.2.2023",
+                                                        "pic":"https://cdn.pixabay.com/photo/2017/02/01/22/02/mountain-landscape-2031539_1280.jpg"
                                                             },
                                                             {
                                                                 "task":"clean the flat",
                                                                 "info":"home",
                                                                 "priority":0,
-                                                                "deadline":13.2.2023
+                                                                "deadline":"13.2.2023",
+                                                                "pic":"https://cdn.pixabay.com/photo/2014/02/17/14/28/vacuum-cleaner-268179_1280.jpg"
                                                                     }
 ]`
---
+++ js/script.js
@@ -0,0 +1,63 @@
+let display = document.getElementById("results");
+
+let data = JSON.parse(toDos);
+console.log(data);
+
+//Cards
+for (i = 0; i < data.length; i++) {
+    display.innerHTML += `<div class="card mb-3" style="width: 18rem;">
+    <img src="${data[i].pic}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data[i].task}</h5>
+      <p class="card-text">Info: ${data[i].info}</p>
+      <hr>
+      <span class="card-text prio">Priority: </span><a href="#" class="btn btn-primary priobtn"> ${data[i].priority}</a>
+      <p class="card-text">Deadline: ${data[i].deadline}</p>
+      <hr>
+      <a href="#" class="btn btn-primary donebtn">Done</a>
+      <a href="#" class="btn btn-danger deletebtn">Delete</a>
+    </div>
+  </div>`;
+}
+
+
+//Prio
+
+let priobtns = document.getElementsByClassName("priobtn");
+
+for (let i = 0; i < priobtns.length; i++) {
+    priobtns[i].addEventListener("click", function() {
+        data[i].priority++;
+        // console.log(people[i].like);
+        document.getElementsByClassName("priobtn")[i].innerHTML = data[i].priority;
+    })
+
+}
+
+//Done
+let donebtns = document.getElementsByClassName("donebtn");
+console.log(donebtns);
+
+for (let i = 0; i < donebtns.length; i++) {
+    donebtns[i].addEventListener("click", function() {
+        document.getElementsByClassName("card")[i].style.opacity = 0.25;
+
+        // document.getElementsByClassName("tcolor")[i].style.color = "red";
+
+
+    })
+}
+
+//Delete
+let deletebtns = document.getElementsByClassName("deletebtn");
+console.log(deletebtns);
+
+for (let i = 0; i < donebtns.length; i++) {
+    deletebtns[i].addEventListener("click", function() {
+        document.getElementsByClassName("card")[i].remove();
+
+        // document.getElementsByClassName("tcolor")[i].style.color = "red";
+
+
+    })
+}
