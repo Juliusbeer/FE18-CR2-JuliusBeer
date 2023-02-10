82fc8043beda52d73e456b99225b160a162a6ab3
Julius Beer <julius.beerlin@gmail.com>
2023-02-10 11:39:01 +0100

    creates data

--- js/data.js
+++ js/data.js
@@ -1,5 +1,56 @@
 let toDos = `[
     {
-
-    }
+"task":"take the dog for a walk",
+"info":"go to Prater",
+"priority":0,
+"deadline":12.2.2023
+    },
+    {
+        "task":"go shopping for the week",
+        "info":"at Hofer",
+        "priority":0,
+        "deadline":23.2.2023
+            },
+            {
+                "task":"go to the gym",
+                "info":"Holmesplace",
+                "priority":0,
+                "deadline":25.2.2023
+                    },
+                    {
+                        "task":"cook for tomorrow",
+                        "info":"Lasagne",
+                        "priority":0,
+                        "deadline":16.2.2023
+                            },
+                            {
+                                "task":"Meet Jan",
+                                "info":"Eiles",
+                                "priority":0,
+                                "deadline":24.2.2023
+                                    },
+                                    {
+                                        "task":"Buy a scale",
+                                        "info":"Media Markt",
+                                        "priority":0,
+                                        "deadline":15.2.2023
+                                            },
+                                            {
+                                                "task":"fuel the car",
+                                                "info":"Jet",
+                                                "priority":0,
+                                                "deadline":12.2.2023
+                                                    },
+                                                    {
+                                                        "task":"Go for a hike",
+                                                        "info":"Rax",
+                                                        "priority":0,
+                                                        "deadline":15.2.2023
+                                                            },
+                                                            {
+                                                                "task":"clean the flat",
+                                                                "info":"home",
+                                                                "priority":0,
+                                                                "deadline":13.2.2023
+                                                                    }
 ]`
