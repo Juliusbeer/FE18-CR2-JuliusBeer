7961d2b6b616d61be1dfd1aa06f28d7fb3b332ba
Julius Beer <julius.beerlin@gmail.com>
2023-02-10 15:39:30 +0100

    adds prio color

--- js/script.js
+++ js/script.js
@@ -3,22 +3,40 @@ let display = document.getElementById("results");
 let data = JSON.parse(toDos);
 console.log(data);
 
+// //Due
+// let due= document.getElementsByClassName("duebtn");
+
+// for(i=0;i<data.length;i++){
+//     if(data[i].deadline<=Date()){
+
+//     }
+// }
+
+
+
+
 //Cards
 for (i = 0; i < data.length; i++) {
-    display.innerHTML += `<div class="card mb-3" style="width: 18rem;">
+    if (data[i].priority <= 1) {
+        display.innerHTML += `<div class="card mb-3" style="width: 18rem;">
     <img src="${data[i].pic}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${data[i].task}</h5>
       <p class="card-text">Info: ${data[i].info}</p>
       <hr>
-      <span class="card-text prio">Priority: </span><a href="#" class="btn btn-primary priobtn"> ${data[i].priority}</a>
+      <span class="card-text prio">Priority: </span><a href="#" class="btn priobtn btn-success"> ${data[i].priority}</a>
       <p class="card-text">Deadline: ${data[i].deadline}</p>
       <hr>
-      <a href="#" class="btn btn-primary donebtn">Done</a>
+      <a href="#" class="btn btn-success donebtn">Done</a>
       <a href="#" class="btn btn-danger deletebtn">Delete</a>
     </div>
   </div>`;
+    }
 }
+
+
+
+
 
 
 //Prio
@@ -30,6 +48,14 @@ for (let i = 0; i < priobtns.length; i++) {
         data[i].priority++;
         // console.log(people[i].like);
         priobtns[i].innerHTML = data[i].priority;
+        if (data[i].priority <= 1) {
+            priobtns[i].style.backgroundColor = "green";
+        } else if (data[i].priority <= 3) {
+            priobtns[i].style.backgroundColor = "yellow";
+            priobtns[i].style.color = "black";
+        } else {
+            priobtns[i].style.backgroundColor = "red";
+        }
 
     })
 
@@ -42,10 +68,6 @@ console.log(donebtns);
 for (let i = 0; i < donebtns.length; i++) {
     donebtns[i].addEventListener("click", function() {
         document.getElementsByClassName("card")[i].style.opacity = 0.25;
-
-        // document.getElementsByClassName("tcolor")[i].style.color = "red";
-
-
     })
 }
 
